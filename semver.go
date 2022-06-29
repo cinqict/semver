c91533bf2bd039f0cc12c10076a41a265936c9ea
Dirc <e.e.cornet@gmail.com>
2022-06-29 21:31:29 +0200

    add: status + branch method

--- semver.go
+++ semver.go
@@ -6,23 +6,42 @@ import (
 	"os/exec"
 )
 
-// func Status() string {
-// 	gitStatus := cmd.NewCmd("git", "status")
-// 	return gitStatus
-// }
-
-// func main() {
-// 	gitStatus, err := exec.Command("git").Output()
-// 	if err != nil {
-// 		fmt.Println(err)
-// 	}
-// 	fmt.Printf(gitStatus)
-// }
-
-func main() {
-	gitStatus, err := exec.Command("git", "status").Output()
+func Status() []byte {
+	status, err := exec.Command("git", "status").Output()
 	if err != nil {
 		log.Fatal(err)
 	}
+	return status
+}
+
+func Branch() []byte {
+	branch, err := exec.Command("git", "--no-pager", "branch", "--show-current").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return branch
+}
+
+// func Log() []byte {
+// 	log, err := exec.Command("git", "--no-pager", "log", "--oneline").Output()
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	return log
+// }
+
+// - [ ]  log
+// - [x] current branch
+// - [ ]  tags
+// - [ ]  count commits
+// - [ ]  latest tag
+// - [ ]  commits since tag
+
+func main() {
+	gitStatus := Status()
 	fmt.Printf(string(gitStatus))
+
+	branch := Branch()
+	fmt.Printf(string(branch))
+
 }
